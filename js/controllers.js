6a88fd5b65650e3f88b34464a106e4c25f9611f8
Kevin Ngo <ngoke@mozilla.com>
2013-05-19 01:13:08 -0700

    dummy date data

--- js/controllers.js
+++ js/controllers.js
@@ -10,8 +10,4 @@ angular.module('LucidApp')
 .controller('EditCtrl', ['$scope', 'EntryService',
                          function ($scope, EntryService) {
     $scope.entries = EntryService.get();
-    $scope.entries = [
-        {sleep: 1, wake: 2},
-        {sleep: 2, wake: 3}
-    ];
 }]);
--- js/services.js
+++ js/services.js
@@ -2,7 +2,23 @@ angular.module('LucidApp', [])
 
 
 .service('EntryService', function() {
-    var entries = [];
+    // var entries = [];
+    var entries = [
+        {sleep: new Date('May 15, 2013 00:00:00'),
+         wake: new Date('May 15, 2013 08:00:00')},
+        {sleep: new Date('May 15, 2013 23:00:00'),
+         wake: new Date('May 16, 2013 07:00:00')},
+        {sleep: new Date('May 16, 2013 23:00:00'),
+         wake: new Date('May 17, 2013 07:00:00')},
+        {sleep: new Date('May 17, 2013 23:00:00'),
+         wake: new Date('May 18, 2013 07:00:00')},
+        {sleep: new Date('May 18, 2013 23:00:00'),
+         wake: new Date('May 19, 2013 07:00:00')},
+        {sleep: new Date('May 19, 2013 23:00:00'),
+         wake: new Date('May 20, 2013 07:00:00')},
+        {sleep: new Date('May 20, 2013 23:00:00'),
+         wake: new Date('May 21, 2013 07:00:00')},
+    ];
     return {
         get: function() {
             return entries;
