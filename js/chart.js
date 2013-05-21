f4e3a063e7a7c3b100430c914d0be5df407d3e50
Kevin Ngo <ngoke@mozilla.com>
2013-05-21 02:38:58 -0700

    work on padding, remove unnecessary elements

--- js/chart.js
+++ js/chart.js
@@ -3,6 +3,7 @@ var PeriodBarChart = function() {
 
     var chart;
     var chartGroup;
+    var axisGroup;
     var element;
 
     var cfg = {
@@ -15,13 +16,14 @@ var PeriodBarChart = function() {
         },
         rect: {
             color: '#299283',
-            padding: 10,
         },
         xAxis: {
-            title: 'Date'
+            title: 'Date',
+            padding: 20
         },
         yAxis: {
-            title: 'Time'
+            title: 'Time',
+            padding: 10
         }
     };
 
@@ -49,31 +51,34 @@ var PeriodBarChart = function() {
 
 
     function draw(element) {
-        var widthPadding = cfg.chart.padding * 3;
-        var heightPadding = cfg.chart.padding * 5;
-
         // Create canvas.
         this.chart = d3.select(element)
             .append('svg:svg')
-            .attr('width', cfg.chart.width + widthPadding)
+            .attr('width', cfg.chart.width)
             .attr('height', cfg.chart.height);
 
         // Create scales.
         var xScale = d3.scale.linear()
             .domain([0, cfg.data.length])
-            .range([0, cfg.chart.width]);
+            .range([0, cfg.chart.width - cfg.yAxis.padding]);
 
         var yScale = d3.scale.linear()
             .domain([0, DAY_SECS])
-            .range([0, cfg.chart.height]);
+            .range([0, cfg.chart.height - cfg.xAxis.padding]);
+
+        axisGroup = this.chart.append('g')
+            .attr('transform',
+                  'translate(' + cfg.yAxis.padding + ', ' +
+                   cfg.xAxis.padding + ')');
 
         // Group elements in the chart together to help padding.
         chartGroup = this.chart.append('g')
             .attr('transform',
-                  'translate(' + cfg.chart.padding * 3 + ', ' +
-                   cfg.chart.padding * 2 + ')');
+                  'translate(' + cfg.yAxis.padding + ', ' +
+                   cfg.xAxis.padding + ')');
 
         // Bars.
+        var rectWidth = (cfg.chart.width - cfg.yAxis.padding) / cfg.data.length;
         var rects = chartGroup.selectAll('rect')
             .data(cfg.data)
             .enter()
@@ -84,7 +89,7 @@ var PeriodBarChart = function() {
                 return yScale((d.wake.getTime() - d.sleep.getTime()) / 1000);
             })
             .attr('opacity', cfg.rect.opacity)
-            .attr('width', cfg.chart.width / cfg.data.length - 20)
+            .attr('width', rectWidth)
             .attr('x', function(d, i) {
                 return xScale(i);
             })
@@ -92,40 +97,8 @@ var PeriodBarChart = function() {
                 return cfg.chart.height - yScale(DAY_SECS - _daySecs(d.sleep));
             });
 
-        // Chart title.
-        if (cfg.chart.title) {
-            chartGroup.append('svg:text')
-                .attr('class', 'title')
-                .text(cfg.chart.title)
-                .attr('x', cfg.chart.width / 2)
-                .attr('y', -1 * cfg.chart.padding)
-                .attr('text-anchor', 'middle');
-        }
-
-        // X axis title.
-        if (cfg.xAxis.title) {
-            chartGroup.append('svg:text')
-                .attr('class', 'xAxisTitle')
-                .text(cfg.xAxis.title)
-                .attr('x', cfg.chart.width / 2)
-                .attr('y', cfg.chart.height + cfg.chart.padding * 2)
-                .attr('text-anchor', 'middle');
-        }
-
-        // Y axis title.
-        if (cfg.yAxis.title) {
-            chartGroup.append('svg:text')
-                .attr('class', 'yAxisTitle')
-                .text(cfg.yAxis.title)
-                // Rotate, X<->Y.
-                .attr('x', -1 * cfg.chart.height / 2)
-                .attr('y', -2 * cfg.chart.padding)
-                .attr('text-anchor', 'middle')
-                .attr('transform', 'rotate(-90)');
-        }
-
         // X axis text.
-        chartGroup.selectAll('text.xAxis')
+        axisGroup.selectAll('text.xAxis')
             .data(cfg.data)
             .enter()
             .append('svg:text')
@@ -134,21 +107,20 @@ var PeriodBarChart = function() {
                 return d[0];
             })
             .attr('x', function(d, i) {
-                return xScale(i) + cfg.rect.width / 2;
+                return xScale(i) + rectWidth / 2;
             })
 
             .attr('text-anchor', 'middle');
 
         // Y axis text.
-        chartGroup.selectAll('text.yAxis')
+        axisGroup.selectAll('text.yAxis')
             .data(hourArray(0, 24, 3))
             .enter()
             .append('svg:text')
             .attr('class', 'yAxis')
             .text(function(d, i) {
-                return d / 60 / 60 + ':00';
+                return d / 60 / 60;
             })
-            .attr('x', -1.25 * cfg.chart.padding)
             .attr('y', function(d) {
                 return yScale(d);
             });
